c65f5797ac588bf43f10f3442080633ca45e95be
balez4464 <71140813+balez4464@users.noreply.github.com>
2020-10-02 10:53:51 -0400

    Update Main.java
    had to manually commit it

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program allows user to enter a  number than caluclates the sum from 1 to that number
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates Scanner
+    Scanner input = new Scanner(System.in);
+
+    //asks user for a number
+    System.out.println("Please enter an integer to count down to");
+
+    //creates intergers
+    int usersNumber = input.nextInt();
+    //for adding to base number everytime
+    int accumulator = 1;
+    //sum of consecutive numbers
+    int sum = 0;
+
+    //while loop
+    while(accumulator <= usersNumber){
+      //finding sum of consecutive numbers
+      sum = sum + accumulator;
+      //adding to the accumulator for next time
+      accumulator = accumulator + 1;
+    }
+    System.out.println("The sum from 1 - " + usersNumber + " is " + sum);
+
   }
 }
